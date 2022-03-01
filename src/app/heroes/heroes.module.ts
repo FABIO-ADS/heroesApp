91d8dc7075726e7babe1f6bdeb5be9ad2cc8ff44
FABIO ADS <fabiobiotec17@gmail.com>
2022-03-01 13:54:06 -0300

    Routes Children

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,13 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
+import { HeroesRoutingModule } from './heroes-routing.module';
 import { AgregarComponent } from './pages/agregar/agregar.component';
 import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListarComponent } from './pages/listar/listar.component';
-
-
 
 @NgModule({
   declarations: [
@@ -15,10 +13,11 @@ import { ListarComponent } from './pages/listar/listar.component';
     BuscarComponent,
     HeroeComponent,
     HomeComponent,
-    ListarComponent
+    ListarComponent,
   ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    HeroesRoutingModule
+  ],
 })
-export class HeroesModule { }
+export class HeroesModule {}
