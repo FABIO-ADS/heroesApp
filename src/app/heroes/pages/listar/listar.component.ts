ba9d1e14ea9d67b3b382f6bf1059a4cbf885cf92
FABIO ADS <fabiobiotec17@gmail.com>
2022-03-01 18:30:49 -0300

    Interface Heroes

--- src/app/heroes/pages/listar/listar.component.ts
+++ src/app/heroes/pages/listar/listar.component.ts
@@ -1,5 +1,6 @@
 import { HeroesService } from './../../services/heroes.service';
 import { Component, OnInit } from '@angular/core';
+import { Heroe } from '../../interfaces/heroes.interfaces';
 
 @Component({
   selector: 'app-listar',
@@ -9,11 +10,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarComponent implements OnInit {
 
+  heroes: Heroe[] = [];
+
   constructor( private heroesService: HeroesService ) { }
 
   ngOnInit(): void {
-    this.heroesService.getHeroes().subscribe(
-      resp => console.log(resp));
+    this.heroesService.getHeroes().subscribe( heroes => this.heroes = heroes );
   }
 
 }
